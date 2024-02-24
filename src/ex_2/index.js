60ae9fdce4026b00ea8e712d8d07cb499d952842
GABRIEL BEZERRA DE SOUSA <gabriel.sousa234>
2024-02-23 22:49:23 -0300

    aula_props 20/02/2024

---
+++ src/ex_2/index.js
@@ -0,0 +1,11 @@
+import{View, Text } from 'react-native';
+
+export default function Props ({mensagem}) {
+    return(
+        <View>
+            <Text> {mensagem}</Text>
+
+            
+        </View>
+    );
+}
