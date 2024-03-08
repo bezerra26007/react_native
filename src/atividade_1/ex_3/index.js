1da146022017d8aa20b6de9590b6ef933ee421ae
GABRIEL BEZERRA DE SOUSA <gabriel.sousa234>
2024-03-08 20:42:26 -0300

    aula_08/03/2024

--- src/atividade_1/ex_3/index.js
+++ src/atividade_1/ex_3/index.js
@@ -1,12 +1,43 @@
+ import React, {useState} from 'react';
+ 
  import {View, Text, TouchableOpacity} from 'react-native';
+
 
  import styles from './styles';
 
 
  export default function Exemplo_3 () {
+
+
+    const [numero, setNumero] = useState(0);
+
+    function Ola () {
+        alert (`olá ${nome}`);
+    };
+
+    function incremento () {
+        const inc = numero + 1;
+        setNumero(inc);
+        //console.log(numero);
+    }
+
     return(
         <View style={styles.container}>
-            <Text> Exemplo_3</Text>
+            <Text style={styles.titulo}> Exemplo_3</Text>
+
+        <TouchableOpacity style={styles.botao} onPress={()=> Ola('Gabriel')}>
+
+        <Text style={styles.txtBotao} >botão</Text>   
+        </TouchableOpacity>
+        <text style={styles.titulo}> Valor: {numero}</text>
+
+        <TouchableOpacity style={styles.botao} onPress={()=> incremento()}>
+
+<Text style={styles.txtBotao} >+ 1</Text>   
+</TouchableOpacity>
+       
+        
+
         </View>
     );
  }
--- src/atividade_1/ex_3/styles.js
+++ src/atividade_1/ex_3/styles.js
@@ -6,6 +6,30 @@ const styles = StyleSheet.create({
         backgroundColor: '#f5f5f5',
         width: '100%',
         alignItems: 'center',
+    },
+    titulo  : {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin : 8,
+        marginbottom: 20,
+
+    },
+
+    botao: {
+        width: '70%',
+        padding: 16,
+        backgroundColor: '#000',
+        color: '#f5f5f5',
+        fontsize:20,
+        borderRadius: 20,
+        alignItems:'center'
+        
+
+    },
+
+    txtBotao: {
+        color: '#f5f5f5',
+        fontSize:20,
     }
 });
 
