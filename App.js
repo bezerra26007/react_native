73a7fb13838b2c14278dc595ff9fe44323b752f9
GABRIEL BEZERRA DE SOUSA <gabriel.sousa234>
2024-03-27 20:35:17 -0300

    aula_27/03

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //import Exemplo_3 from './src/atividade_1/ex_3';
 //import Atividade_3 from './src/atividade_3';
-import Exemplo4 from './src/ex_4';
+//import Exemplo4 from './src/ex_4';
+import Atividade_4 from './src/atividade_4';
+
 
 //import Atividade_3 from './src/atividade_1';
 
@@ -10,7 +12,7 @@ import Exemplo4 from './src/ex_4';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo4 />
+      <Atividade_4 />
       <StatusBar style="auto" />
     </View>
   );
--- src/atividade_4/index.js
+++ src/atividade_4/index.js
@@ -3,7 +3,7 @@ import { View, Text, TextInput, Pressable } from 'react-native';
 
 import styles from './styles';
 
-export default function atividade_4() {
+export default function Atividade_4() {
 
     const [isFocus, setIsFocus] = useState(false);
 
@@ -14,30 +14,35 @@ export default function atividade_4() {
     function blurHandler() {
         setIsFocus(false);
     }
-
-    const [mensagem, setMensagem] = useState('Sua mensagem aqui!');
-    const [mens2, setMens2] = useState('Outra mensagem');
+    
+    const [mensagem, setMensagem] = useState('');
+    const [mens, setMens] = useState('');
+    const [mens2, setMens2] = useState('');
     const [mensTela, setMensTela] = useState('');
+    const [mensTela1, setMensTela1] = useState('');
+
 
     function atualizaMensagemHandler() {
-        setMensTela(mens2);
+        setMensTela('');
+        setMensTela1();
+        setMens('')
         setMens2('');
+
+
+        
     }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.titulo}>atividade_4</Text>
+            <Text style={styles.titulo}>Atividade_4</Text>
 
             <Text style={styles.txtSaida}>{mensagem}</Text>
 
-            <TextInput
-                style={isFocus ? { borderWidth: 3, borderColor: '#0f0' } : { borderWidth: 3, borderColor: '#00f' }}
-                placeholder='Digite o texto aqui...'
-                onChangeText={(txt) => setMensagem(txt)}
-                onFocus={() => focusHandler()}
-                onBlur={() => blurHandler()}
-            />
+            
 
+
+            
+<Text style={styles.titulo}>Nome</Text>
             <Text style={styles.txtSaida}>{mensTela}</Text>
             <TextInput
                 style={styles.txtEntrada}
@@ -45,6 +50,19 @@ export default function atividade_4() {
                 onChangeText={(entrada) => setMens2(entrada)}
                 value={mens2}
             />
+
+
+<Text style={styles.titulo}>Sobrenome</Text>
+
+<Text style={styles.txtSaida}>{mensTela}</Text>
+            <TextInput
+                style={styles.txtEntrada}
+                // placeholder='Outro texto' 
+                onChangeText={(entrada) => setMens(entrada)}
+                value={mens}
+            />
+
+            
             <Pressable
                 onPress={() => atualizaMensagemHandler()}
                 style={styles.botao}
